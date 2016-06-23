ae234470b77a9b59bbe83665297ccbe381336438
sabrinaluo <luohaojing@gmail.com>
2016-06-24 00:39:27 +0800

    crawler error handler

--- api/one/index.js
+++ api/one/index.js
@@ -8,29 +8,44 @@ const router = express.Router();
 const root = 'http://wufazhuce.com';
 
 router.route('/')
-  .get((req, res) => {
+  .get((req, res, next) => {
     let homeCrawler = new crawler.Home(root);
-    homeCrawler.get().then(data => {
-      res.json(data);
-    });
+    homeCrawler
+      .get()
+      .then(data => {
+        res.json(data);
+      })
+      .catch(e=> {
+        next(e);
+      });
   });
 
 router.route('/question/:id')
-  .get((req, res) => {
+  .get((req, res, next) => {
     let url = root + '/question/' + req.params.id;
     let questionCrawler = new crawler.Question(url);
-    questionCrawler.get().then(data => {
-      res.json(data);
-    });
+    questionCrawler
+      .get()
+      .then(data => {
+        res.json(data);
+      })
+      .catch(e=> {
+        next(e);
+      });
   });
 
 router.route('/article/:id')
-  .get((req, res) => {
+  .get((req, res, next) => {
     let url = root + '/article/' + req.params.id;
     let articleCrawler = new crawler.Question(url);
-    articleCrawler.get().then(data => {
-      res.json(data);
-    });
+    articleCrawler
+      .get()
+      .then(data => {
+        res.json(data);
+      })
+      .catch(e=> {
+        next(e)
+      });
   });
 
 module.exports = router;
