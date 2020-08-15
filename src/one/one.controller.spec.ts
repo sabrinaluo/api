00c5ad314de3f80dffac40ce694c8dfe0000eb60
sabrinaluo <luohaojing@gmail.com>
2020-08-15 23:38:55 +0800

    test: fix test

--- src/one/one.controller.spec.ts
+++ src/one/one.controller.spec.ts
@@ -1,19 +1,25 @@
+import { HttpModule } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { OneController } from './one.controller';
+import { OneService } from './one.service';
 
 describe('One Controller', () => {
-  let controller: OneController;
+  let oneController: OneController;
+  let oneService: OneService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       controllers: [OneController],
+      providers: [OneService],
     }).compile();
 
-    controller = module.get<OneController>(OneController);
+    oneController = moduleRef.get<OneController>(OneController);
+    oneService = await moduleRef.resolve(OneService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(oneController).toBeDefined();
   });
 });
--- src/one/one.service.spec.ts
+++ src/one/one.service.spec.ts
@@ -1,19 +1,45 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import * as fs from 'fs';
 
+import { HttpModule, HttpService } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as cheerio from 'cheerio';
+import { of } from 'rxjs';
+
+import * as homeData from './__fixtures__/home.json';
 import { OneService } from './one.service';
 
 describe('OneService', () => {
-  let service: OneService;
+  let oneService: OneService;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [OneService],
     }).compile();
 
-    service = module.get<OneService>(OneService);
+    oneService = module.get<OneService>(OneService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(oneService).toBeDefined();
+  });
+
+  it('should get convert homepage html to json', async () => {
+    const html = fs
+      .readFileSync(__dirname + '/__fixtures__/home.html')
+      .toString('utf-8');
+    jest.spyOn(httpService, 'get').mockImplementationOnce(() =>
+      of({
+        data: cheerio.load(html, { decodeEntities: false }),
+        status: 200,
+        statusText: 'OK',
+        headers: {},
+        config: {},
+      }),
+    );
+    const result = await oneService.getHome();
+    expect(result).toEqual(homeData);
   });
 });
